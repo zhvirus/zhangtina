ad793833f9a1a8898fa633621dd8c469fe80ec52
zhvirus <zhvirus@hotmail.com>
2013-12-19 17:54:20 +0800

    Some matrix work, save the progress in repo.

--- TinaMath/src/Matrix.cpp
+++ TinaMath/src/Matrix.cpp
@@ -26,10 +26,28 @@ namespace ZH{
                    VALUE_EQUAL<T>(t[14], m.v[14]) && VALUE_EQUAL<T>(t[15], m.v[15])
                    ;
         }
+        template <class T>
+        bool operator == ( T t, const Matrix<T>& m )
+        {
+            return VALUE_EQUAL<T>(t, m.v[0]) && VALUE_EQUAL<T>(t, m.v[1]) &&
+                   VALUE_EQUAL<T>(t, m.v[2]) && VALUE_EQUAL<T>(t, m.v[3]) &&
+                   VALUE_EQUAL<T>(t, m.v[4]) && VALUE_EQUAL<T>(t, m.v[5]) &&
+                   VALUE_EQUAL<T>(t, m.v[6]) && VALUE_EQUAL<T>(t, m.v[7]) &&
+                   VALUE_EQUAL<T>(t, m.v[8]) && VALUE_EQUAL<T>(t, m.v[9]) &&
+                   VALUE_EQUAL<T>(t, m.v[10]) && VALUE_EQUAL<T>(t, m.v[11]) &&
+                   VALUE_EQUAL<T>(t, m.v[12]) && VALUE_EQUAL<T>(t, m.v[13]) &&
+                   VALUE_EQUAL<T>(t, m.v[14]) && VALUE_EQUAL<T>(t, m.v[15])
+                   ;
+        }
 
         // !=
         template <class T>
         bool operator != ( const T* const t, const Matrix<T>& m )
+        {
+            return !(t==m);
+        }
+        template <class T>
+        bool operator != ( T t, const Matrix<T>& m )
         {
             return !(t==m);
         }
@@ -38,6 +56,8 @@ namespace ZH{
         // ---------------------------------
         // class function
         // ---------------------------------
+
+        // constructor
         template<class T>
         Matrix<T>::Matrix()
         {
@@ -47,26 +67,122 @@ namespace ZH{
         template<class T>
         Matrix<T>::Matrix( const Matrix<T>& m)
         {
-            memcpy((void*)v, (void*)m.v, sizeof(T)*16);
+            *this = m;
         }
 
         template<class T>
         Matrix<T>::Matrix( const T* const t)
         {
-            memcpy((void*)v, (void*)t, sizeof(T)*16);
+            *this = t;
         }
 
         template<class T>
-        bool Matrix<T>::operator == ( const Matrix<T>& m) const
+        Matrix<T>::Matrix( T t)
+        {
+            *this = t;
+        }
+
+        //---------------------------
+        // =
+        //---------------------------
+        template<class T>
+        Matrix<T>& Matrix<T>::operator = ( const Matrix<T>& m )
+        {
+            memcpy((void*)v, (void*)m.v, sizeof(T)*16);
+            return *this;
+        }
+        Matrix<T>& Matrix<T>::operator = ( const T* const t )
+        {
+            memcpy((void*)v, (void*)t, sizeof(T)*16);
+            return *this;
+        }
+        Matrix<T>& Matrix<T>::operator = ( T t )
+        {
+            for(unsigned int i=0;i<16;i++){
+                v[i] = t;
+            }
+            return *this;
+        }
+
+        //---------------------------
+        // ==
+        //---------------------------
+        template<class T>
+        bool Matrix<T>::operator == ( const Matrix<T>& m ) const
         {
             return v == m;
         }
-
         template<class T>
-        bool Matrix<T>::operator != ( const Matrix<T>& m) const
+        bool Matrix<T>::operator == ( const T* const t ) const
+        {
+            return t == *this;
+        }
+        template<class T>
+        bool Matrix<T>::operator == ( T t ) const
+        {
+            return t == *this;
+        }
+
+        //---------------------------
+        // !=
+        //---------------------------
+        template<class T>
+        bool Matrix<T>::operator != ( const Matrix<T>& m ) const
         {
             return v != m;
         }
+        template<class T>
+        bool Matrix<T>::operator != ( const T* const t ) const
+        {
+            return t != *this;
+        }
+        template<class T>
+        bool Matrix<T>::operator != ( T t ) const
+        {
+            return t != *this;
+        }
+
+        //---------------------------
+        // +
+        //---------------------------
+        template<class T>
+        Matrix<T> Matrix<T>::operator + ( const Matrix<T>& m ) const
+        {
+            T data[16];
+            for(unsigned int i=0;i<16;i++){
+                data[i] = v[i] + m.v[i];
+            }
+            return Matrix<T>(data);
+        }
+        Matrix<T> Matrix<T>::operator + ( const T* const t ) const
+        {
+            T data[16];
+            for(unsigned int i=0;i<16;i++){
+                data[i] = v[i] + t[i];
+            }
+            return Matrix<T>(data);
+        }
+        Matrix<T> Matrix<T>::operator + ( T t ) const
+        {
+            T data[16];
+            for(unsigned int i=0;i<16;i++){
+                data[i] = v[i] + t;
+            }
+            return Matrix<T>(data);
+        }
+
+        //---------------------------
+        // -
+        //---------------------------
+
+        //---------------------------
+        // *
+        //---------------------------
+
+        //---------------------------
+        // \
+        //---------------------------
+
 
         template<class T>
         void Matrix<T>::identityIt()
--- includes/Math/Matrix.h
+++ includes/Math/Matrix.h
@@ -8,7 +8,9 @@
         ZH_TEMPLATE_DLL template class ZH_DLL Matrix<CLASS> ; \
         typedef Matrix<CLASS> CLASS_MATRIX; \
         ZH_TEMPLATE_DLL template ZH_DLL bool operator == ( const CLASS* const, const Matrix<CLASS>& ); \
-        ZH_TEMPLATE_DLL template ZH_DLL bool operator != ( const CLASS* const, const Matrix<CLASS>& );
+        ZH_TEMPLATE_DLL template ZH_DLL bool operator == ( CLASS, const Matrix<CLASS>& ); \
+        ZH_TEMPLATE_DLL template ZH_DLL bool operator != ( const CLASS* const, const Matrix<CLASS>& ); \
+        ZH_TEMPLATE_DLL template ZH_DLL bool operator != ( CLASS, const Matrix<CLASS>& );
 #endif
 
 namespace ZH{
@@ -17,15 +19,60 @@ namespace ZH{
         template <class T>
         class Matrix{
         public:
+            ~Matrix(){}
             Matrix();
             Matrix( const Matrix& );
             Matrix( const T* const );
-            ~Matrix(){}
+            Matrix( T );
 
+            // inline
+            inline T& operator [](unsigned int i){ return v[i];}
+            inline T& operator [][](unsigned int i,unsigned int j){ return v[i*4+j];}
+
+            // =
+            Matrix& operator = ( const Matrix& );
+            Matrix& operator = ( const T* const );
+            Matrix& operator = ( T );
+            // ==
             bool operator == ( const Matrix& ) const;
+            bool operator == ( const T* const ) const;
+            bool operator == ( T ) const;
+            // !=
             bool operator != ( const Matrix& ) const;
+            bool operator != ( const T* const ) const;
+            bool operator != ( T ) const;
+            // +
+            Matrix operator+(const Matrix&) const;
+            Matrix operator+(const T* const) const;
+            Matrix operator+(T)const;
+            // -
+            Matrix operator-(const Matrix&) const;
+            Matrix operator-(const T* const) const;
+            Matrix operator-(T) const;
+            // *
+            Matrix operator*(const Matrix&) const;
+            Matrix operator*(const T* const) const;
+            Matrix operator*(T) const;
+            // /
+            Matrix operator/(const Matrix&) const;
+            Matrix operator/(const T* const) const;
+            Matrix operator/(T) const;
 
+            // http://en.wikipedia.org/wiki/Matrix_(mathematics)
+            //
             void identityIt();
+            void transposeIt();
+            bool inverseIt();
+            bool inverseMatrix( Matrix& )const;
+            
+            bool isDiagonal()const;
+            bool isUpperTriangular()const;
+            bool isLowerTriangular()const;
+            bool isIdentity()const;
+            bool isInvertible()const;
+            bool isOrthogonal()const;
+
+            T traceValue()const;
         public:
             T v[16];
         };
@@ -33,9 +80,13 @@ namespace ZH{
         // ==
         template <class T>
         bool operator == ( const T* const, const Matrix<T>& );
+        template <class T>
+        bool operator == ( T, const Matrix<T>& );
         // !=
         template <class T>
         bool operator != ( const T* const, const Matrix<T>& );
+        template <class T>
+        bool operator != ( T, const Matrix<T>& );
 
         // Exports
         common_matrix_export_defines( float,  matrix_f );
